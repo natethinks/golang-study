79e617207726693f7de55876a3fdae4b37009ec7
Nate Meyers <natejmeyers@gmail.com>
2020-10-08 03:17:11 +0000

    stuff

--- main.go
+++ main.go
@@ -5,9 +5,19 @@ import (
     "net/http"
 )
 
+// comments in go are just two slashes
+
+// so this is a really simple web server. Everything you visit on the web is just a server you're making a rqeuest to and that server sends back a response. This server only has 2 options for what it will do, and they're accessed by the two http.HandleFunc() functions near the bottom.
+
 func hello(w http.ResponseWriter, req *http.Request) {
 
+    // Before I messed with this it was just a "string literal" now its a variable, and that variable is also a string. Functionally, it's the same, it's just an organization trick
+
+    // string literal style
     fmt.Fprintf(w, "hello\n")
+
+    // variable style
+    fmt.Fprintf(w, something)
 }
 
 func headers(w http.ResponseWriter, req *http.Request) {
@@ -26,3 +36,6 @@ func main() {
 
     http.ListenAndServe(":8090", nil)
 }
+
+// see right here the <!DOCTYPE html>, so the browser knows its html
+const something string = `ur mom`
